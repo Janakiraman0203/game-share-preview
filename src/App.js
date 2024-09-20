0367cde8e70e127a83a2a6b9ec02f86709641e58
Janakiraman0203 <janakiraman@letsterra.com>
2024-09-20 13:27:34 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Helmet>
+        <meta property="og:title" content="Your Page Title Here" />
+        <meta property="og:description" content="A brief description of the page." />
+        <meta property="og:image" content="https://dxovcbhhspgz7.cloudfront.net/game-icons-mvp5/beam-battles-icon.png" />
+        <meta property="og:url" content="https://client-webview-staging-b.letsterra.com/game/share" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Your Website Name" />
+      </Helmet>
+      <div className="flex items-center justify-center">
+        <div className="font-bold text-5xl text-white">Hello</div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
