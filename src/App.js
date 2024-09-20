66e468244dbd687e52e207265655b2c1a06dd338
Janakiraman0203 <janakiraman@letsterra.com>
2024-09-20 13:32:42 +0530

    3

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
   return (
     <div>
       <Helmet>
-        <meta property="og:title" content="Your Page Title Here" />
-        <meta property="og:description" content="A brief description of the page." />
+        <meta property="og:title" content="janaki" />
+        <meta property="og:description" content="janaki-111" />
         <meta property="og:image" content="https://dxovcbhhspgz7.cloudfront.net/game-icons-mvp5/beam-battles-icon.png" />
         <meta property="og:url" content="https://client-webview-staging-b.letsterra.com/game/share" />
         <meta property="og:type" content="website" />
