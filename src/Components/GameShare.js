b1c58acf8d6fe6b50080e63eedf7e5fc32d43855
Janakiraman0203 <janakiraman@letsterra.com>
2024-09-23 18:26:32 +0530

    15

--- src/Components/GameShare.js
+++ src/Components/GameShare.js
@@ -5,28 +5,7 @@ import { useParams } from 'react-router-dom';
 function GameShare() {
     const { id } = useParams()
     const [gameDetail, setGameDetails] = useState({})
-    const openAppOrRedirect = () => {
-        const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-        return
-        // For iOS devices
-        if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-            setTimeout(() => {
-                window.location.href = "https://apps.apple.com/in/app/terra-by-terrabyte/id6450105686";
-            }, 2000);
-        } else if (/android/i.test(userAgent)) {
-            setTimeout(() => {
-                window.location.href = "https://play.google.com/store/apps/details?id=com.terrabyte.terra";
-            }, 2000);
-        } else {
-              setTimeout(()=>{
-                window.location.href = "https://play.google.com/store/apps/details?id=com.terrabyte.terra";
-              },2000) 
-        }
-    };
 
-    // useEffect(() => {
-    //     openAppOrRedirect()
-    // }, [])
 
     useEffect(() => {
         (async () => {
@@ -76,13 +55,6 @@ function GameShare() {
                         <p className="text-lg text-gray-300">{gameDetail?.timesPlayed}</p>
                     </div>
                 </div>
-
-                <button
-                    className="bg-blue-500 text-white py-2 px-6 mt-8 rounded-md hover:bg-blue-600 transition"
-                    onClick={openAppOrRedirect}
-                >
-                    Open App or Redirect
-                </button>
             </div>
         </div>
                 </div>}
