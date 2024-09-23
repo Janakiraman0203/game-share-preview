06d103791de80e6b81bbd8014d3bbd3b417b36e1
Janakiraman0203 <janakiraman@letsterra.com>
2024-09-23 17:39:05 +0530

    10

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/game/id/:id' element={<GameShare />} />
+        <Route path='/game/:id' element={<GameShare />} />
       </Routes>
     </Router>
   );
